64cd99ed010e8b6bd469f326cff41287ddde0dd5
brickyt <43393476+brickyt@users.noreply.github.com>
2018-09-24 14:04:41 -0500

    Create clock.py

---
+++ clock.py
@@ -0,0 +1,13 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=1)
+def timed_job():
+    print('This job is run every minute.')
+
+@sched.scheduled_job('interval', hours=24)
+def timed_job():
+    print('This job is run every 24 hours.')
+
+sched.start()
